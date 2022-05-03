1b53b951c6afc6f3946a06985c4b109e087a0cb6
Gautami Sinha <gautami.sinha90@gmail.com>
2022-05-03 18:09:02 +0530

    header for small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
-
+const navMenu = document.getElementById('nav_menu');
+    navToggle = document.getElementById('nav_toggle');
+    navClose = document.getElementById('nav_close');
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('showMenu');
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('showMenu');
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
